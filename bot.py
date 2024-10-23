a9d29f3f73f46285dd31f8b0b92f30954871344a
David Páez <96804874+DavidPaBe@users.noreply.github.com>
2024-10-23 08:30:11 -0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send(f'Hello {message.author.name}!')
+
+# Obtén el token del bot desde las variables de entorno
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client.run(TOKEN)
