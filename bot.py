8634c87dc4c56fc56c1b2704e1019beb977cdc41
David Páez <96804874+DavidPaBe@users.noreply.github.com>
2024-10-23 12:53:31 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,44 +4,35 @@ from discord.ext import commands
 import asyncio
 import subprocess
 import sys
-import requests
 
+# Inicializar el bot con los intents necesarios
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+# Evento que se ejecuta cuando el bot está listo
 @bot.event
 async def on_ready():
     print(f'Conectado como {bot.user}!')
-    await bot.load_extension('commands')  # Cargar la extensión
+    
+    # Llamar a la función de conteo de tiempo para apagar el bot después de 3 horas
     await shutdown_after_time()
 
 async def shutdown_after_time():
-    await asyncio.sleep(10800)  # 3 horas en segundos
+    # Esperar 3 horas (10800 segundos)
+    await asyncio.sleep(10800)
     print("Cerrando el bot después de 3 horas.")
-    await trigger_workflow()
+    
+    # Cerrar el bot
     await bot.close()
 
-async def trigger_workflow():
-    url = f"https://api.github.com/repos/DavidPaBe/TecDiscordBot/actions/workflows/124212019/dispatches"
-    headers = {
-        "Authorization": f"token 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",
-        "Accept": "application/vnd.github.v3+json"
-    }
-    data = {
-        "ref": "main"
-    }
-    response = requests.post(url, json=data, headers=headers)
-    if response.status_code == 204:
-        print("Workflow triggered successfully!")
-    else:
-        print(f"Failed to trigger workflow: {response.status_code} - {response.text}")
-
+# Iniciar el bot con el token de las variables de entorno
 if __name__ == "__main__":
     while True:
         try:
             bot.run(os.getenv('DISCORD_TOKEN'))
         except Exception as e:
             print(f"Error al ejecutar el bot: {e}. Reiniciando...")
-            subprocess.Popen([sys.executable, 'bot.py'])  # Reinicia el bot
+            # Reiniciar el script
+            subprocess.Popen([sys.executable, 'bot.py'])
             break
