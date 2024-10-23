bf1bcad3eed485ffa4958142e22f6f59a6981e4d
David Páez <96804874+DavidPaBe@users.noreply.github.com>
2024-10-23 12:57:23 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,48 @@
 import os
 import discord
-from discord.ext import commands
-import commands as bot_commands  # Importamos los comandos desde otro archivo
+from discord.ext import commands, tasks
+import asyncio
 
 # Inicializar el bot con los intents necesarios
 intents = discord.Intents.default()
 intents.message_content = True
-intents.reactions = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Evento que se ejecuta cuando el bot está listo
 @bot.event
 async def on_ready():
     print(f'Conectado como {bot.user}!')
+    
+    # Llamar a la función de conteo de tiempo para apagar el bot después de 3 horas
+    await shutdown_after_time()
 
-# Cargar los comandos desde commands.py
+async def shutdown_after_time():
+    # Esperar 3 horas (10800 segundos)
+    await asyncio.sleep(10800)
+    print("Cerrando el bot después de 3 horas.")
+    
+    # Realiza alguna acción antes de cerrar, si lo necesitas
+    # await some_other_process()
+    trigger_workflow();
+
+    # Cerrar el bot
+    await bot.close()
+
+async def trigger_workflow():
+    url = f"https://api.github.com/repos/DavidPaBe/TecDiscordBot/actions/workflows/124212019/dispatches"
+    headers = {
+        "Authorization": f"token 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",
+        "Accept": "application/vnd.github.v3+json"
+    }
+    data = {
+        "ref": "main"
+    }
+    response = requests.post(url, json=data, headers=headers)
+    if response.status_code == 204:
+        print("Workflow triggered successfully!")
+    else:
+        print(f"Failed to trigger workflow: {response.status_code} - {response.text}")
+
 bot_commands.setup(bot)
 
 # Iniciar el bot con el token de las variables de entorno
