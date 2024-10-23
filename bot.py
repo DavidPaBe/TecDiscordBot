f2abdf40b0a529275915aa1d2deb2bea5433102b
David Páez <96804874+DavidPaBe@users.noreply.github.com>
2024-10-23 12:54:50 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,38 +1,21 @@
 import os
 import discord
 from discord.ext import commands
-import asyncio
-import subprocess
-import sys
+import commands as bot_commands  # Importamos los comandos desde otro archivo
 
 # Inicializar el bot con los intents necesarios
 intents = discord.Intents.default()
 intents.message_content = True
+intents.reactions = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Evento que se ejecuta cuando el bot está listo
 @bot.event
 async def on_ready():
     print(f'Conectado como {bot.user}!')
-    
-    # Llamar a la función de conteo de tiempo para apagar el bot después de 3 horas
-    await shutdown_after_time()
 
-async def shutdown_after_time():
-    # Esperar 3 horas (10800 segundos)
-    await asyncio.sleep(10800)
-    print("Cerrando el bot después de 3 horas.")
-    
-    # Cerrar el bot
-    await bot.close()
+# Cargar los comandos desde commands.py
+bot_commands.setup(bot)
 
 # Iniciar el bot con el token de las variables de entorno
-if __name__ == "__main__":
-    while True:
-        try:
-            bot.run(os.getenv('DISCORD_TOKEN'))
-        except Exception as e:
-            print(f"Error al ejecutar el bot: {e}. Reiniciando...")
-            # Reiniciar el script
-            subprocess.Popen([sys.executable, 'bot.py'])
-            break
+bot.run(os.getenv('DISCORD_TOKEN'))
