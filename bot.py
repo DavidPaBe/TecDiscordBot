b3272f1d5448744a66210b38d1713b081f5e348e
David Páez <96804874+DavidPaBe@users.noreply.github.com>
2024-10-23 12:46:53 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,34 +8,32 @@ import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)  # El prefijo está aquí
+
+# Cargar los comandos desde el archivo commands.py
+bot.load_extension('commands')
 
 @bot.event
 async def on_ready():
     print(f'Conectado como {bot.user}!')
-    
     await shutdown_after_time()
 
 async def shutdown_after_time():
-    # Cambiado a 3 horas
-    await asyncio.sleep(10800)  # 3 horas en segundos
+    await asyncio.sleep(5)  # 3 horas en segundos
     print("Cerrando el bot después de 3 horas.")
-    
     await trigger_workflow()
-    
     await bot.close()
 
 async def trigger_workflow():
     url = f"https://api.github.com/repos/DavidPaBe/TecDiscordBot/actions/workflows/124212019/dispatches"
     headers = {
-        "Authorization": f"token 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",
+        "Authorization": f"token {os.getenv('TOKEN_GITHUB')}",
         "Accept": "application/vnd.github.v3+json"
     }
     data = {
         "ref": "main"
     }
     response = requests.post(url, json=data, headers=headers)
-    
     if response.status_code == 204:
         print("Workflow triggered successfully!")
     else:
