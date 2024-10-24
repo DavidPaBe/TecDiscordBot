8a365b03c50849b653d463d65e4e273017da5ca8
David Páez <96804874+DavidPaBe@users.noreply.github.com>
2024-10-23 23:57:56 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,7 +37,7 @@ async def on_ready():
 
 async def shutdown_after_time():
     # Esperar 3 horas (10800 segundos)
-    await asyncio.sleep(10800)
+    await asyncio.sleep(10)
     print("Cerrando el bot después de 3 horas.")
     
     # Realiza alguna acción antes de cerrar, si lo necesitas
