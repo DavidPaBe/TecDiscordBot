e0204fed60ca9220394bd17c26a85e5d30c3b4eb
David Páez <96804874+DavidPaBe@users.noreply.github.com>
2024-10-23 23:59:36 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,23 +14,6 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     print(f'Conectado como {bot.user}!')
-
-# Cargar los comandos desde commands.py
-bot_commands.setup(bot)
-
-# Iniciar el bot con el token de las variables de entorno
-bot.run(os.getenv('DISCORD_TOKEN'))
-
-
-# Inicializar el bot con los intents necesarios
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-# Evento que se ejecuta cuando el bot está listo
-@bot.event
-async def on_ready():
-    print(f'Conectado como {bot.user}!')
     
     # Llamar a la función de conteo de tiempo para apagar el bot después de 3 horas
     await shutdown_after_time()
